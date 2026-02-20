acfaf3dfceb8b0d20c102c5e7de12332ea78852c
jahidhasan-webdev01 <jahidhasan.webdev01@gmail.com>
2026-02-20 23:47:19 +0600

    initial filter and dynamic count done

--- script.js
+++ script.js
@@ -2,20 +2,42 @@ document.getElementById("filter-section")
     .addEventListener("click", function (event) {
         const clickedId = event.target.id;
         const currentFilter = document.getElementById("current-filter");
+
+        // sections 
+        const allPost = document.getElementById("all-posts");
+        const thrivingPosts = document.getElementById("thriving-posts");
+        const stragglingPosts = document.getElementById("straggling-posts");
+
         if (clickedId == "btn-all-filter") {
             removeAllFilterButtonDesign();
             currentFilter.innerText = "All";
             addFilterButtonDesign("btn-all-filter");
+
+            thrivingPosts.classList.add("hidden");
+            stragglingPosts.classList.add("hidden");
+            allPost.classList.remove("hidden");
         }
         else if (clickedId == "btn-thriving-filter") {
             removeAllFilterButtonDesign();
             currentFilter.innerText = "Thriving";
             addFilterButtonDesign("btn-thriving-filter");
 
+            stragglingPosts.classList.add("hidden");
+            allPost.classList.add("hidden");
+            thrivingPosts.classList.remove("hidden");
+
+            render(thrivingPosts, thrivingPlants);
+
         } else if (clickedId == "btn-straggling-filter") {
             removeAllFilterButtonDesign();
             currentFilter.innerText = "Straggling";
             addFilterButtonDesign("btn-straggling-filter");
+
+            thrivingPosts.classList.add("hidden");
+            allPost.classList.add("hidden");
+            stragglingPosts.classList.remove("hidden");
+
+            render(stragglingPosts, stragglingPlants);
         }
     })
 
@@ -35,7 +57,7 @@ const thrivingPlants = [];
 const stragglingPlants = [];
 
 // Event Delegation
-document.querySelector("#posts").addEventListener("click", function (event) {
+document.querySelector(".posts").addEventListener("click", function (event) {
     if (event.target.classList.contains("btn-make-thriving")) {
         handleThriving(event);
     }
@@ -86,6 +108,8 @@ function handleThriving(e) {
             plantExist.status = "thriving";
         }
     }
+
+    count();
 }
 
 function handleStraggling(e) {
@@ -129,6 +153,8 @@ function handleStraggling(e) {
             plantExist.status = "straggling";
         }
     }
+
+    count();
 }
 
 function disableStraggling(e) {
@@ -150,3 +176,54 @@ function disableThriving(e) {
     parent.querySelector(".btn-make-thriving").classList.remove("cursor-not-allowed");
     parent.querySelector(".btn-make-thriving").classList.add("cursor-pointer");
 }
+
+function render(sectionEl, data) {
+    sectionEl.innerHTML = '';
+
+    data.forEach(plant => {
+        console.log(plant)
+        const newPost = document.createElement("div");
+
+        newPost.innerHTML = `
+        <div class="bg-white p-8 rounded-lg shadow-2xl">
+                <img src="./assests/maple-leaf.webp" alt="maple-leaf" class="rounded-lg w-full h-60 object-cover">
+
+                <div class="mt-4 space-y-2">
+                    <h1 class="text-xl font-bold">${plant.name}</h1>
+
+                    <p>Type:
+                        <span id="type" class="bg-amber-400 text-white px-3 py-0.5 rounded-full text-sm">
+                            ${plant.type}
+                        </span>
+                    </p>
+
+                    <p>Status:
+                        <span id="status"
+                            class="lowercase bg-black px-3 py-0.5 font-bold text-white rounded-full text-sm">
+                            ${plant.status}
+                        </span>
+                    </p>
+
+                    <p class="text-neutral-500">
+                        Plantation Date: <span id="plantation-date">${plant.plantationDate}</span>
+                    </p>
+                </div>
+            </div>
+        `
+
+        sectionEl.appendChild(newPost);
+
+    })
+
+
+}
+
+function count() {
+    const totalCount = document.getElementById("all-posts").children.length;
+
+    document.getElementById("total-count").innerText = totalCount;
+    document.getElementById("thriving-count").innerText = thrivingPlants.length;
+    document.getElementById("straggling-count").innerText = stragglingPlants.length;
+}
+
+count();
